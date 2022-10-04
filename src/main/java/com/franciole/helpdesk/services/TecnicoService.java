05bf92768b7f997c5439a66f60062fad3c05b9ab
franciole <franciole.bezerra@gmail.com>
2022-10-03 21:33:24 -0300

    endpoint create para Tecnico

--- src/main/java/com/franciole/helpdesk/services/TecnicoService.java
+++ src/main/java/com/franciole/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.franciole.helpdesk.domain.Tecnico;
+import com.franciole.helpdesk.domain.dtos.TecnicoDTO;
 import com.franciole.helpdesk.domain.repositories.TecnicoRepository;
 import com.franciole.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,11 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }
