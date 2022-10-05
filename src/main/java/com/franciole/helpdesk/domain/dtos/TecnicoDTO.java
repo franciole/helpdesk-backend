26d9b4065615cb3c546fecfe39aafbd9e65ff417
franciole <franciole.bezerra@gmail.com>
2022-10-04 23:12:49 -0300

    validacao dos campos de criacao de novo usuario

--- src/main/java/com/franciole/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/franciole/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.franciole.helpdesk.domain.Tecnico;
 import com.franciole.helpdesk.domain.enums.Perfil;
@@ -15,9 +17,13 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected Integer id;
+	@NotNull(message = "Campo Nome é requerido")
 	protected String nome;
+	@NotNull(message = "Campo CPF é requerido")
 	protected String cpf;
+	@NotNull(message = "Campo EMAIL é requerido")
 	protected String email;
+	@NotNull(message = "Campo SENHA é requerido")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 	@JsonFormat(pattern = "dd/MM/yyyy")
--- src/main/java/com/franciole/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/franciole/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,12 +21,24 @@ public class ResourceExceptionHandler {
 				"Object Not Found", ex.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
-	
+
 	@ExceptionHandler(DataIntegrityViolationException.class)
-	public ResponseEntity<StandarError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
+	public ResponseEntity<StandarError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+			HttpServletRequest request) {
 		StandarError error = new StandarError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
 				"Violação de dados", ex.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandarError> validatorErros(MethodArgumentNotValidException ex, HttpServletRequest request) {
+		ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"validation error", "Erro na validação dos campos", request.getRequestURI());
+
+		for (FieldError x : ex.getBindingResult().getFieldErrors()) {
+			errors.addError(x.getField(), x.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 
 }
