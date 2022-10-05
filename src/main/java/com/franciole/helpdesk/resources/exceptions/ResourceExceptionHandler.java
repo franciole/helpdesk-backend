af9cf01904e2956239cbf259052606c749944d83
franciole <franciole.bezerra@gmail.com>
2022-10-04 22:30:29 -0300

    execao para CPF e E-Mail ja cadastrados

--- src/main/java/com/franciole/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/franciole/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.franciole.helpdesk.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,6 +18,13 @@ public class ResourceExceptionHandler {
 		StandarError error = new StandarError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
 				"Object Not Found", ex.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandarError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
+		StandarError error = new StandarError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Violação de dados", ex.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 
 }
--- src/main/java/com/franciole/helpdesk/services/TecnicoService.java
+++ src/main/java/com/franciole/helpdesk/services/TecnicoService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import com.franciole.helpdesk.domain.Pessoa;
 import com.franciole.helpdesk.domain.Tecnico;
 import com.franciole.helpdesk.domain.dtos.TecnicoDTO;
+import com.franciole.helpdesk.domain.repositories.PessoaRepository;
 import com.franciole.helpdesk.domain.repositories.TecnicoRepository;
 import com.franciole.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -16,6 +19,8 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
@@ -28,7 +33,19 @@ public class TecnicoService {
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCpfEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
+	}
+
+	private void validaPorCpfEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() &&  obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() &&  obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
 	}
 }
